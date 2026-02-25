be211501282844a3eb7687838568fe2e9fd0b675
cavanluo666 <cavanluo666@gmail.com>
2026-02-25 06:57:05 +0000

    deploy: b1bc98ce2955705620151f4782bb386e8c498ff5

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/25/github action hexo 自动部署/","2026/02/25/hello-world/"];function toRandomPost(){
+var posts=["2026/02/25/github action hexo 自动部署/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
