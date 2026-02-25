c306adf0c047e3e5a3ab18ac877705349f544c0c
cavanluo666 <cavanluo666@gmail.com>
2026-02-25 04:37:28 +0000

    deploy: 81adc94fc69167c659f38ad8d7f234c1cb3a2eb8

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/25/hello-world/"];function toRandomPost(){
+var posts=["2026/02/25/hello-world/","2026/02/25/测试/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
