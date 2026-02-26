10fc5e3d9955968a6856285928ff20431ee57d86
cavanluo666 <cavanluo666@gmail.com>
2026-02-26 07:46:41 +0000

    deploy: 4264ebe2b44021c26d21ded9ebd9d38b3767e621

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/25/github action hexo 自动部署/"];function toRandomPost(){
+var posts=["2026/02/25/github action hexo 自动部署/","2026/02/26/qexo搭建教程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
