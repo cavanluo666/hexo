28115311afa5bd0efff377828b981e11dcb2252a
cavanluo666 <cavanluo666@gmail.com>
2026-06-13 08:34:09 +0000

    deploy: 6955a1e79563e24a4541ae4aea894fc1c2f10d0f

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/25/github action hexo 自动部署/","2026/02/26/qexo搭建教程/"];function toRandomPost(){
+var posts=["2026/06/13/P1909 [NOIP 2016 普及组] 买铅笔题解/","2026/02/26/qexo搭建教程/","2026/02/25/github action hexo 自动部署/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
